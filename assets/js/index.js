4a2a207e3a0cb70005b3455f078c7c6ef096bf50
shootermv <shootermv@gmali.com>
2018-07-03 10:13:08 +0300

    add eract code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,26 @@
-console.log('hi!!!')
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+        console.log('you pressed "IN"')
+      // return 'You liked this.';
+    }
+
+    return e(
+      'i',
+      { onClick: () => this.setState({ liked: true }), className:'fa fa-linkedin' },
+      ''
+    );
+  }
+}
+
+const domContainer = document.querySelector('.social.list-inline li:nth-child(1) a');
+ReactDOM.render(e(LikeButton), domContainer);
